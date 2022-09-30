536f11167a24bb5041909a1f0f8a020c6986889e
lily <46290459+OutrightWings@users.noreply.github.com>
2022-09-29 23:25:54 -0700

    Working Item Model Gen

---
+++ src/main/java/com/outrightwings/extended_horse_armor/datagen/DataGenerators.java
@@ -0,0 +1,19 @@
+package com.outrightwings.extended_horse_armor.datagen;
+
+import com.outrightwings.extended_horse_armor.ExtendedArmorMain;
+import net.minecraft.data.DataGenerator;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.forge.event.lifecycle.GatherDataEvent;
+
+@Mod.EventBusSubscriber(modid = ExtendedArmorMain.MOD_ID, bus = Mod.EventBusSubscriber.Bus.MOD)
+public class DataGenerators {
+    @SubscribeEvent
+    public static void gatherData(GatherDataEvent event){
+        DataGenerator generator = event.getGenerator();
+        ExistingFileHelper existingFileHelper = event.getExistingFileHelper();
+
+        generator.addProvider(new ModItemModelProvider(generator,existingFileHelper));
+    }
+}
---
+++ src/main/java/com/outrightwings/extended_horse_armor/datagen/ModItemModelProvider.java
@@ -0,0 +1,31 @@
+package com.outrightwings.extended_horse_armor.datagen;
+
+import com.outrightwings.extended_horse_armor.ExtendedArmorMain;
+import com.outrightwings.extended_horse_armor.items.RegisterItems;
+import net.minecraft.data.DataGenerator;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.client.model.generators.ItemModelBuilder;
+import net.minecraftforge.client.model.generators.ItemModelProvider;
+import net.minecraftforge.common.data.ExistingFileHelper;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItemModelProvider extends ItemModelProvider {
+
+    public ModItemModelProvider(DataGenerator generator, ExistingFileHelper existingFileHelper) {
+        super(generator, ExtendedArmorMain.MOD_ID, existingFileHelper);
+    }
+
+    @Override
+    protected void registerModels() {
+        for (RegistryObject<Item> item : RegisterItems.ITEMS.getEntries()) {
+            simpleItem(item.get());
+        }
+    }
+
+    private ItemModelBuilder simpleItem(Item item) {
+        return withExistingParent(item.getRegistryName().getPath(),
+                new ResourceLocation("item/generated")).texture("layer0",
+                new ResourceLocation(ExtendedArmorMain.MOD_ID,"item/" + item.getRegistryName().getPath()));
+    }
+}
