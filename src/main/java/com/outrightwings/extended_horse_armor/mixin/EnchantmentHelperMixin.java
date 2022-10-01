252494d83df90fee45c6edef53779c185968eae9
lily <46290459+OutrightWings@users.noreply.github.com>
2022-09-30 21:14:21 -0700

    Combined File

--- src/main/java/com/outrightwings/extended_horse_armor/mixin/EnchantmentHelperMixin.java
+++ src/main/java/com/outrightwings/extended_horse_armor/mixin/EnchantmentHelperMixin.java
@@ -1,7 +1,10 @@
 package com.outrightwings.extended_horse_armor.mixin;
 
-import com.outrightwings.extended_horse_armor.enchantable.EnchantableHorseArmor;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.animal.horse.AbstractHorse;
+import net.minecraft.world.item.HorseArmorItem;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.enchantment.Enchantment;
 import net.minecraft.world.item.enchantment.EnchantmentHelper;
 import org.spongepowered.asm.mixin.Mixin;
@@ -9,10 +12,18 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
+import static net.minecraft.world.item.enchantment.EnchantmentHelper.getItemEnchantmentLevel;
+
 @Mixin(EnchantmentHelper.class)
 public class EnchantmentHelperMixin {
     @Inject(method = "getEnchantmentLevel(Lnet/minecraft/world/item/enchantment/Enchantment;Lnet/minecraft/world/entity/LivingEntity;)I", at = @At("HEAD"), cancellable = true)
     private static void getEnchantmentLevel(Enchantment enchantmentIn, LivingEntity entityIn, CallbackInfoReturnable<Integer> cir){
-            EnchantableHorseArmor.checkHorseArmorEnchantmentLevelHook(enchantmentIn,entityIn,cir);
+        if(entityIn instanceof AbstractHorse){
+            ItemStack armor = ((AbstractHorse)entityIn).getItemBySlot(EquipmentSlot.CHEST);
+            if (armor.getItem() instanceof HorseArmorItem) {
+                int level = getItemEnchantmentLevel(enchantmentIn, armor);
+                cir.setReturnValue(level);
+            }
+        }
     }
 }
